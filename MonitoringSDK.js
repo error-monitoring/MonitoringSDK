937c5f9ee46b092b2025f06e044600e824950b34
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-20 14:22:55 +0800

    add

--- MonitoringSDK.js
+++ MonitoringSDK.js
@@ -2,7 +2,7 @@
  * @Author: wenquan.huang 
  * @Date: 2018-11-15 13:28:51 
  * @Last Modified by: wq599263163@163.com
- * @Last Modified time: 2018-11-20 14:15:33
+ * @Last Modified time: 2018-11-20 14:16:04
  */
 
 import {
@@ -25,7 +25,6 @@ class MonitoringSDK {
     constructor({
         debug = true,
     } ={}) {
-        console.log(11)
         this.debug = debug
         this.initSDK()
     }
