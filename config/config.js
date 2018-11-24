d896a5cff369369b3c676f490757f316014a4500
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-24 04:32:51 -0800

    修改配置

--- config/config.js
+++ config/config.js
@@ -2,13 +2,11 @@
  * @Author: wenquan.huang 
  * @Date: 2018-11-16 19:13:34 
  * @Last Modified by: wq599263163@163.com
- * @Last Modified time: 2018-11-23 15:17:47
+ * @Last Modified time: 2018-11-24 04:30:52
  */
 
 
-
-// const base = 'http://47.98.132.24:7401'
-const base = 'http://192.168.255.135:7401'
+const base = 'http://192.168.31.60:7401'
 
 // const base = 'http://192.168.8.103:7401'
 
