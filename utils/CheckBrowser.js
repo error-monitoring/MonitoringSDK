ffd0bb5480654445ac4fc11c05a2337d52f9d652
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-24 05:19:13 -0800

    add

--- utils/CheckBrowser.js
+++ utils/CheckBrowser.js
@@ -111,6 +111,7 @@ export class checkBrowser {
 
     getOsInfo() {
         const userAgent = this.userAgent;
+        console.log(userAgent,'userAgent')
         let name = '';
         let version = "";
         if (userAgent.includes("win")) {
