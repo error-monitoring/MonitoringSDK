c63e812f616e2ccfc9a04a5a06f5630c063223fd
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-24 05:30:06 -0800

    add

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@
  * @Author: wenquan.huang 
  * @Date: 2018-11-16 19:13:34 
  * @Last Modified by: wq599263163@163.com
- * @Last Modified time: 2018-11-24 04:30:52
+ * @Last Modified time: 2018-11-24 05:30:02
  */
 
 
-const base = 'http://192.168.31.60:7401'
+// const base = 'http://192.168.31.60:7401'
 
-// const base = 'http://192.168.8.103:7401'
+const base = 'http://47.98.132.24:7401'
 
 
 export {
