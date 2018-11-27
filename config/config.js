4cd6d4422ff509fe6c58dde54aaa839eb3cf0b93
huangwenquan <wenquan.huang@magicwindow.cn>
2018-11-27 15:25:54 +0800

    test-commit

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@
  * @Author: wenquan.huang 
  * @Date: 2018-11-16 19:13:34 
  * @Last Modified by: wq599263163@163.com
- * @Last Modified time: 2018-11-27 00:36:40
+ * @Last Modified time: 2018-11-27 15:25:41
  */
 
 
-const base = 'http://minecraft.cssandjs.com'
+// const base = 'http://minecraft.cssandjs.com'
 
-// const base = 'http://47.98.132.24:7001'
+const base = 'http://192.168.255.162:7001'
 
 
 export {
